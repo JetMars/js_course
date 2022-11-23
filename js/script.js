9dd1666ba106e7c6050184d30843b0193259d3fb
JetMars <imanager666@gmail.com>
2022-11-23 10:36:49 +0300

    add questions

--- js/script.js
+++ js/script.js
@@ -1,4 +1,20 @@
-const a = 'Hello world';
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const movieA = prompt('Один из просмотренных фильмов?', '');
+const ratingB = prompt('На сколько оцените его?', '');
+const movieC = prompt('Один из просмотренных фильмов?', '');
+const ratingD = prompt('На сколько оцените его?', '');
+
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
+};
 
 
-console.log(2 % 5);
+personalMovieDB.movies[movieA] = ratingB;
+personalMovieDB.movies[movieC] = ratingD;
+
+console.log(personalMovieDB);
