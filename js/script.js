bfd21cb7b0af4d313f54f2812ce6bb0e2a6495bd
JetMars <imanager666@gmail.com>
2022-11-26 15:32:13 +0300

    add function

--- js/script.js
+++ js/script.js
@@ -1,9 +1,6 @@
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+'use strict';
 
-const movieA = prompt('Один из просмотренных фильмов?', '');
-const ratingB = prompt('На сколько оцените его?', '');
-const movieC = prompt('Один из просмотренных фильмов?', '');
-const ratingD = prompt('На сколько оцените его?', '');
+let numberOfFilms;
 
 const personalMovieDB = {
   count: numberOfFilms,
@@ -13,8 +10,65 @@ const personalMovieDB = {
   privat: false,
 };
 
+function start() {
+  numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-personalMovieDB.movies[movieA] = ratingB;
-personalMovieDB.movies[movieC] = ratingD;
+  while (isNaN(numberOfFilms) || numberOfFilms === 0) {
+    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+  }
 
-console.log(personalMovieDB);
+  personalMovieDB.count = numberOfFilms;
+}
+
+function remberMyFilms() {
+  for (let i = 0; i < 2; i++) {
+    const movieA = prompt('Один из просмотренных фильмов?', '');
+    const ratingB = prompt('На сколько оцените его?', '');
+
+    if ((+movieA !== 0) && (+ratingB !== 0) && movieA.length < 50) {
+      personalMovieDB.movies[movieA] = ratingB;
+    } else {
+      i--;
+    }
+  }
+}
+
+function dectedPersonalLevel(number) {
+
+  if (number < 10) {
+    console.log('Просморенно довольно мало фильмов');
+  } else if (number <= 30) {
+    console.log('Вы классический зритель');
+  } else if (number > 30) {
+    console.log('Вы киноман');
+  } else {
+    console.log('Произошла ошибка');
+  }
+
+}
+
+function showMyDB(hidden) {
+  if (!hidden) {
+    console.log(personalMovieDB);
+  }
+}
+
+function writeYourGenres() {
+  let count = 0;
+  for (let i = 0; i < 3; i++) {
+    count++;
+    const genre = prompt(`Ваш любимый жанр под номером ${count}`);
+    if ((+genre !== 0) && isNaN(+genre)) {
+      personalMovieDB.genres.push(genre);
+    } else {
+      count--;
+      i--;
+    }
+  }
+}
+
+start();
+remberMyFilms();
+dectedPersonalLevel(personalMovieDB.count);
+showMyDB(personalMovieDB.privat);
+writeYourGenres();
